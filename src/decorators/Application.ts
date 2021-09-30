49656109c322e5372a471a62909e573e40114b01
Daniel Hammerl <daniel.hammerl99@gmx.de>
2021-09-30 19:05:34 +0200

    class

--- src/decorators/Application.ts
+++ src/decorators/Application.ts
@@ -1,9 +1,11 @@
-import express from 'express';
-import config from 'config';
+import express, { Express } from 'express';
 
+process.env.SUPPRESS_NO_CONFIG_WARNING = 'y';
+
+import config from 'config';
 import { critical, debug, error, info } from '../internalUtils/logging';
 import { setConfigDefaults } from '../internal/setConfigDefaults';
-import { _initOrm, getOrm } from '../utils/getORM';
+import { _initOrm } from '../utils/getORM';
 import { MikroORMOptions } from '@mikro-orm/core/utils/Configuration';
 import { MikroORM, RequestContext } from '@mikro-orm/core';
 
@@ -17,53 +19,24 @@ export interface ApplicationMetaData {
  * there can also be a "beforeStart" function to run initial code before startup
  * @constructor
  */
-// eslint-disable-next-line @typescript-eslint/ban-types
-export function Application(metaData?: ApplicationMetaData): (constructor: Function) => void {
-  // eslint-disable-next-line @typescript-eslint/ban-types
-  return async function (constructor: Function) {
+export class BaseApplication {
+  dbEntities: MikroORMOptions['entities'] = [];
+
+  constructor() {
     const useDatabase = config.has('database.type') && config.has('database.url');
-    let orm: MikroORM | null = null;
 
     if (useDatabase) {
       debug('Connecting to database ...');
-      if (!metaData?.mikroOrmEntities) {
+      if (this.dbEntities.length === 0) {
         // TODO should be suppressable
         console.warn('No mikro orm entities specified');
       }
-      orm = await _initOrm(metaData?.mikroOrmEntities ?? []);
-    }
-
-    if (typeof constructor?.prototype?.main !== 'function') {
-      error("Error in Application decorator! Class must have a method 'main'");
-      process.exit(1);
-    }
-
-    if (typeof constructor?.prototype?.beforeStart === 'function') {
-      debug('Run before start handler ...');
-      constructor.prototype.beforeStart();
-    }
-    debug('Starting Application ...');
-    debug('Set default configuration ...');
-    setConfigDefaults();
-
-    const app = express();
-
-    const webserverPort = config.get<number>('webserver.port');
-    const webserverHost = config.get<string>('webserver.host');
-
-    if (useDatabase && orm) {
-      app.use((req, res, next) => {
-        // @ts-expect-error hää?
-        RequestContext.create(orm.em, next);
+      _initOrm(this.dbEntities ?? []).then((orm) => {
+        this._appStartUp(orm);
       });
+    } else {
+      this._appStartUp();
     }
-
-    // TODO add catch
-    new Promise(constructor.prototype.main(app)).then(() => {
-      app.listen(webserverPort, webserverHost, () => {
-        info(`Webserver started on ${webserverHost}:${webserverPort}`);
-      });
-    });
 
     // eslint-disable-next-line @typescript-eslint/no-empty-function
     const tearDownApplication = async () => {};
@@ -88,6 +61,41 @@ export function Application(metaData?: ApplicationMetaData): (constructor: Funct
     process.on('SIGQUIT', () => {
       tearDownApplication().finally(() => {
         process.exit(0);
+      });
+    });
+  }
+
+  // @ts-expect-error
+  public main = (app: Express): Promise<void> => {
+    //error("Error in Application decorator! Class must have a method 'main'");
+    //process.exit(1);
+  };
+
+  protected beforeStart = () => {};
+
+  private _appStartUp = (orm?: MikroORM) => {
+    debug('Run before start handler ...');
+    this.beforeStart();
+
+    debug('Starting Application ...');
+    debug('Set default configuration ...');
+    setConfigDefaults();
+
+    const app = express();
+
+    const webserverPort = config.get<number>('webserver.port');
+    const webserverHost = config.get<string>('webserver.host');
+
+    if (orm) {
+      app.use((req, res, next) => {
+        RequestContext.create(orm.em, next);
+      });
+    }
+
+    // TODO add catch
+    this.main(app).then(() => {
+      app.listen(webserverPort, webserverHost, () => {
+        info(`Webserver started on ${webserverHost}:${webserverPort}`);
       });
     });
   };
