2d534161757ff445638c1786654ca47e5ffdab99
Daniel Hammerl <daniel.hammerl99@gmx.de>
2021-11-12 21:58:20 +0100

    make db connection failures breaking and close service

--- src/database/getORM.ts
+++ src/database/getORM.ts
@@ -8,8 +8,8 @@ let orm: MikroORM | null = null;
 export const _initOrm = async (entities: MikroORMOptions['entities']): Promise<MikroORM> => {
   const databaseConfig = getConfig<{ type: 'mysql'; url: string }>('database');
   if (!databaseConfig) {
-    // TODO error handling
-    throw new Error('database is not configured correctly');
+    log('critical', 'database is not configured correctly');
+    process.exit(1);
   }
 
   // TODO what if we pass the wrong db type as config?
@@ -21,8 +21,8 @@ export const _initOrm = async (entities: MikroORMOptions['entities']): Promise<M
     });
     return orm;
   } catch (e) {
-    log('error', 'Failed initializing database connection');
-    throw e;
+    log('critical', 'Failed initializing database connection', e as Error);
+    process.exit(1);
   }
 };
 
