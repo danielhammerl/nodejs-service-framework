3c8deec49ba8cc313a7dff01632576ff613d6bed
Daniel Hammerl <daniel.hammerl99@gmx.de>
2021-12-11 00:18:02 +0100

    maybe fixed bug or so, but made it definitely better

--- src/internal/serviceName.ts
+++ src/internal/serviceName.ts
@@ -1,9 +1,16 @@
-let _serviceName: string;
+import { log } from '../logging';
+
+let _serviceName: string | undefined;
 
 export const setServiceName = (serviceName: string): void => {
   _serviceName = serviceName;
 };
 
 export const getServiceName = (): string => {
+  if (typeof _serviceName === 'undefined') {
+    log('error', 'Trying to access service name, but no name is set');
+    throw new Error('Trying to access service name, but no name is set');
+  }
+
   return _serviceName;
 };
