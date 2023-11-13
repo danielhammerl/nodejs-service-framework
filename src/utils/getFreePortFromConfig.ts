5ba5e34a8c1714f116b5824cadb89a0b4c7f4318
danielhammerl <daniel.hammerl99@gmx.de>
2023-11-13 10:30:42 +0100

    fix: webserver.port config can now also be a string ( and with that a env variable )

--- src/utils/getFreePortFromConfig.ts
+++ src/utils/getFreePortFromConfig.ts
@@ -3,10 +3,10 @@ import { getRandomNumber } from './randomNumber';
 
 /**
  * receives one or multiple ports and return the ( first ) port which is free and not already in use,
- * if there isnt any free port, returns 0;
+ * if there isn't any free port, returns 0;
  * @param config
  */
-export const getFreePortFromConfig = async (config: number | number[] | undefined): Promise<number> => {
+export const getFreePortFromConfig = async (config: number | number[] | string | undefined): Promise<number> => {
   if (Array.isArray(config)) {
     for (let index = 0; index < config.length; index++) {
       if (!(await tcpPortUsed.check(config[index]))) {
@@ -15,11 +15,12 @@ export const getFreePortFromConfig = async (config: number | number[] | undefine
     }
 
     return 0;
-  } else if (typeof config === 'number') {
-    if (await tcpPortUsed.check(config)) {
+  } else if (typeof config === 'number' || typeof config === 'string') {
+    const port = typeof config === 'number' ? config : Number.parseInt(config);
+    if (await tcpPortUsed.check(port)) {
       return 0;
     }
-    return config;
+    return port;
   } else {
     let number: number | undefined;
     do {
